77b9403de4316e661b1c8f48f4287b60320b4f4a
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-13 00:23:37 +0300

    Updates

---
+++ build/precache-manifest.df02c61a767cf0db6ba1cc2017931acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d206a70a3696de81fbcfe1c36b1916",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "e92bc3d7dfb5da78bb61",
+    "url": "/lights-out/static/css/main.5c2695e6.chunk.css"
+  },
+  {
+    "revision": "eeec4fddedb9d9cd12f5",
+    "url": "/lights-out/static/js/2.98a32157.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out/static/js/2.98a32157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92bc3d7dfb5da78bb61",
+    "url": "/lights-out/static/js/main.cc4a8c54.chunk.js"
+  },
+  {
+    "revision": "be864883fe966990f17a",
+    "url": "/lights-out/static/js/runtime-main.67c84857.js"
+  },
+  {
+    "revision": "cb433c610cf806ae6fb929c4113423d0",
+    "url": "/lights-out/static/media/Neon.cb433c61.ttf"
+  }
+]);
